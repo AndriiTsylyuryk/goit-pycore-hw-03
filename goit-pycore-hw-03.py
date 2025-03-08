c744fd174323affe4e06ff50993c958b69923ef1
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2025-03-08 12:02:16 +0200

    Update goit-pycore-hw-03.py

--- goit-pycore-hw-03.py
+++ goit-pycore-hw-03.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def get_days_from_today(date):
     try:
@@ -9,7 +9,7 @@ def get_days_from_today(date):
     except Exception as e:
         return f'Error: {e}'
 
-print(get_days_from_today('1997-07-31'))
+print(get_days_from_today('2028-07-31'))
 
 # ////////////////////////////////////////////////////////////
 import random
@@ -21,12 +21,13 @@ def get_numbers_ticket(min, max, quantity):
         while len(num_list) < quantity:
             random_number = random.randint(min,max)
             if random_number not in num_list:
-                num_list.append(random_number)  
+                num_list.append(random_number)
+    else: print('Quantity cannot be larger than max') 
 
     num_list.sort()
     return num_list
 
-lottery_numbers = get_numbers_ticket(1, 5, 6)
+lottery_numbers = get_numbers_ticket(1, 10, 11)
 print("Your numbers:", lottery_numbers)
 
 
@@ -46,46 +47,38 @@ def normalize_phone(phone_number):
 #/////////////////////////////////////////////////////////////
 
 def get_upcoming_birthdays(users):
-    dates_list = []
+    
+    try:
+        dates_list = []
+        today = datetime.today().date()
+        for user in users:
+            formatted_birthday=datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+            birthday_this_year = formatted_birthday.replace(year=today.year)
+            seven_days = birthday_this_year - today
+            if (seven_days.days >= 0 and seven_days.days <= 7) and birthday_this_year.weekday() != 5 and birthday_this_year.weekday() != 6:
+                item = {'name': user['name'], 'congrat_dat': birthday_this_year.strftime("%Y.%m.%d")}
+                dates_list.append(item)
+            elif (seven_days.days >= 0 and seven_days.days <= 7) and birthday_this_year.weekday() == 5: 
+                new_date = birthday_this_year + timedelta(days=2)
+                item = {'name': user['name'], 'congrat_dat': new_date.strftime("%Y.%m.%d")}
+                dates_list.append(item)
+            elif (seven_days.days >= 0 and seven_days.days <= 7) and birthday_this_year.weekday() == 6:
+                new_date = birthday_this_year + timedelta(days=1)
+                item = {'name': user['name'], 'congrat_dat': new_date.strftime("%Y.%m.%d")}
+                dates_list.append(item)
 
-    today = datetime.today().date()
+        return dates_list  
 
-    print(today)
-
-    for user in users:
-       formatted_birthday=datetime.strptime(user["birthday"], "%Y.%m.%d").date()
-       birthday_this_year = formatted_birthday.replace(year=today.year)
-       weekday = birthday_this_year.weekday()
-       seven_days = birthday_this_year - today
-       print(seven_days)
-    #    if birthday_this_year < today:
-    #        pass
-    #    if seven_days <= 7:
-           
-        
-    #    if weekday != 5 and weekday != 6:
-    #        dates_list.append({user['name']})
-          
-
-
-    return dates_list
+    except Exception as e:
+       return f'Error: {e}'
+    
            
      
-
-
-
-
-
-
 users = [
-    {"name": "John Doe", "birthday": "1985.03.08"},
-    {"name": "Jane Smith", "birthday": "1990.03.12"}
+    {"name": "John Doe", "birthday": "1985.03.28"},
+    {"name": "Jane Smith", "birthday": "1990.03.21"}
 ]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
-print("Список привітань на цьому тижні:", upcoming_birthdays)
+print("Congrats list:", upcoming_birthdays)
 
-# [
-#     {'name': 'John Doe', 'congratulation_date': '2024.01.23'}, 
-#     {'name': 'Jane Smith', 'congratulation_date': '2024.01.29'}
-# ]
