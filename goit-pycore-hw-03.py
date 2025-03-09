7dfc06de0609bce371e5f8b0f9bc7034cc19261c
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2025-03-09 12:10:39 +0200

    Update goit-pycore-hw-03.py

--- goit-pycore-hw-03.py
+++ goit-pycore-hw-03.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 def get_days_from_today(date):
     try:
@@ -16,8 +16,9 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     num_list = []
+    difference = max-min
 
-    if min >= 1 and max <= 1000 and quantity <= max:
+    if min >= 1 and max <= 1000 and difference >= quantity:
         while len(num_list) < quantity:
             random_number = random.randint(min,max)
             if random_number not in num_list:
@@ -27,7 +28,7 @@ def get_numbers_ticket(min, max, quantity):
     num_list.sort()
     return num_list
 
-lottery_numbers = get_numbers_ticket(1, 10, 11)
+lottery_numbers = get_numbers_ticket(10, 12, 5)
 print("Your numbers:", lottery_numbers)
 
 
@@ -51,18 +52,24 @@ def get_upcoming_birthdays(users):
     try:
         dates_list = []
         today = datetime.today().date()
+        # today = date(year=2000, month=12, day=31)
         for user in users:
             formatted_birthday=datetime.strptime(user["birthday"], "%Y.%m.%d").date()
             birthday_this_year = formatted_birthday.replace(year=today.year)
-            seven_days = birthday_this_year - today
-            if (seven_days.days >= 0 and seven_days.days <= 7) and birthday_this_year.weekday() != 5 and birthday_this_year.weekday() != 6:
+            days_diff = birthday_this_year - today
+
+            if (days_diff.days < 0):
+                birthday_this_year = formatted_birthday.replace(year=today.year + 1)
+                days_diff = (birthday_this_year - today)
+
+            if (days_diff.days >= 0 and days_diff.days <= 7) and birthday_this_year.weekday() != 5 and birthday_this_year.weekday() != 6:
                 item = {'name': user['name'], 'congrat_dat': birthday_this_year.strftime("%Y.%m.%d")}
                 dates_list.append(item)
-            elif (seven_days.days >= 0 and seven_days.days <= 7) and birthday_this_year.weekday() == 5: 
+            elif (days_diff.days >= 0 and days_diff.days <= 7) and birthday_this_year.weekday() == 5: 
                 new_date = birthday_this_year + timedelta(days=2)
                 item = {'name': user['name'], 'congrat_dat': new_date.strftime("%Y.%m.%d")}
                 dates_list.append(item)
-            elif (seven_days.days >= 0 and seven_days.days <= 7) and birthday_this_year.weekday() == 6:
+            elif (days_diff.days >= 0 and days_diff.days <= 7) and birthday_this_year.weekday() == 6:
                 new_date = birthday_this_year + timedelta(days=1)
                 item = {'name': user['name'], 'congrat_dat': new_date.strftime("%Y.%m.%d")}
                 dates_list.append(item)
@@ -75,8 +82,8 @@ def get_upcoming_birthdays(users):
            
      
 users = [
-    {"name": "John Doe", "birthday": "1985.03.28"},
-    {"name": "Jane Smith", "birthday": "1990.03.21"}
+    {"name": "John Doe", "birthday": "2001.01.02"},
+    {"name": "Jane Smith", "birthday": "1990.01.04"}
 ]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
